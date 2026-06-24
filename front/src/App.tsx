d5f281a45f5976541dc64cea23e4d9bbc09e2dc1
jayreck996 <jayreck996@gmail.com>
2026-06-25 11:36:04 +1200

    feat: wire Badges route into App

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import Home from './pages/Home';
 import Quizzes from './pages/Quizzes';
 import Leaderboard from './pages/Leaderboard';
+import Badges from './pages/Badges';
 import './App.css';
 
 export default function App() {
@@ -12,12 +13,14 @@ export default function App() {
         <NavLink to="/">Home</NavLink>
         <NavLink to="/quizzes">Quizzes</NavLink>
         <NavLink to="/leaderboard">Leaderboard</NavLink>
+        <NavLink to="/badges">Badges</NavLink>
       </nav>
       <main className="container">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/quizzes" element={<Quizzes />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/badges" element={<Badges />} />
         </Routes>
       </main>
     </BrowserRouter>
